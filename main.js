40fcd31bf6960a02adb324b4eb4361aaa785bc12
Anastasia <nastya.kaaaa@mail.ru>
2024-10-23 02:47:49 +0300

    Изменение активного состояния в блоке выбора тура

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
-header = document.querySelector(".header")
-header_logo = document.querySelector(".header__logo").firstElementChild
-intro = document.querySelector(".intro")
-select = document.querySelector(".select")
-
+const header = document.querySelector(".header");
+const headerLogo = document.querySelector(".header__logo").firstElementChild;
+const intro = document.querySelector(".intro");
+const select = document.querySelector(".select");
+const choiceList = document.querySelector('.choice__list');
 // Фиксированная шапка при прокрутке
 window.addEventListener('scroll', function () {
     if (window.scrollY > 450) {
         // Добавляем класс для header
-        header.classList.add("header__fixed")
+        header.classList.add("header__fixed");
         // Изменяем логотип на черный
-        header_logo.src = "./images/YourTour_black.svg"
+        headerLogo.src = "./images/YourTour_black.svg";
         // Изменяем intro чтобы не уползало при фиксированной шапке
-        intro.classList.add("intro__fixed")
+        intro.classList.add("intro__fixed");
     }
     else {
-        header.classList.remove("header__fixed")
-        header_logo.src = "./images/YourTour.svg"
-        intro.classList.remove("intro__fixed")
+        header.classList.remove("header__fixed");
+        headerLogo.src = "./images/YourTour.svg";
+        intro.classList.remove("intro__fixed");
     }
 })
 
 // Select
 select.addEventListener("change", function () {
-    select.style.color = "#1B1F2B"
+    select.style.color = "#1B1F2B";
+})
+
+// Изменение активного состояния в блоке выбора тура
+choiceList.addEventListener("click", e => {
+    let currentActive = choiceList.querySelector('.choice__item-active');
+    if (e.target.tagName === "A" && currentActive != e.target.parentElement) {
+        currentActive.classList.remove("choice__item-active");
+        e.target.parentElement.classList.add("choice__item-active");
+    }
 })
 
